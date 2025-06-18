cc5f52b25f1f4c77b7dda0e81ee3d0faa52f3144
방다혜 <asc8420@naver.com>
2025-06-18 10:48:48 +0900

    2025.06.18

---
+++ src/main/java/com/sist/web/Devops5Application.java
@@ -0,0 +1,17 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 서버위치 
+/*
+ *   maven => Framework => war
+ *   gradle => Boot  => jar (실행 가능 파일) 
+ */
+@SpringBootApplication
+public class Devops5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops5Application.class, args);
+	}
+
+}
